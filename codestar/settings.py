43a195cbe53956d7d71639bef5faad67413a9a5d
Explosion247 <Dave.staward@gmail.com>
2025-12-16 18:28:57 +0000

    Update SECRET_KEY to use environment variable for production security

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8rjmejswh%0_jvv&p^8ttt8=*sbu_&kom-dj_#8))0$z!+fpw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
