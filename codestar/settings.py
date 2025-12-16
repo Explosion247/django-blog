dd71452531bb873ad5c9c9c1d83a6c979ec1f544
Explosion247 <Dave.staward@gmail.com>
2025-12-16 21:56:41 +0000

    Add Post model and initial migration; update admin registration

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,11 +27,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'django-blog-1799-ab048536e8d2.herokuapp.com',
     '127.0.0.1'
+]
+
+# TRUSTED ORIGINS: local and production server URL domain - Allows you to post blog content from the admin dashboard
+CSRF_TRUSTED_ORIGINS = [
+    "https://*django-blog-1799-ab048536e8d2.herokuapp.com",
+    "https://*127.0.0.1:8000/"
 ]
 
 
