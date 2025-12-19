0e543a116193b07df089d08be8250bdf47dede8e
Explosion247 <Dave.staward@gmail.com>
2025-12-19 18:16:24 +0000

    Add CollaborateRequest model and implement collaboration form; enhance About page and post detail view with comment editing functionality

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
@@ -7,10 +9,24 @@ def about_me(request):
     """
     Renders the About Page
     """
+
+    if request.method == "POST":
+        collaborate_form_result = CollaborateForm(data=request.POST)
+        if collaborate_form_result.is_valid():
+            collaborate_form_result.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your Collaboration request has been sent'
+            )
+
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
+
+
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about,
+         'collaborate_form': collaborate_form},
     )
