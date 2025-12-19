4664f626bf94d2fd5965bc1e0daadca209d1ddc3
Explosion247 <Dave.staward@gmail.com>
2025-12-19 12:12:14 +0000

    Add 'about' app with model, views, templates, and URL configuration

--- about/models.py
+++ about/models.py
@@ -5,8 +5,11 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"),(1, "Published"))
 
-class Post(models.Model):
+class About(models.Model):
 
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
