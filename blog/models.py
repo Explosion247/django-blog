7ed10e458c4fdb484193c159d01556de8e975e68
Explosion247 <Dave.staward@gmail.com>
2025-12-19 22:12:02 +0000

    Refactor imports and enhance model documentation; update templates for improved comment handling

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,10 @@ STATUS = ((0, "Draft"),(1, "Published"))
 
 class Post(models.Model):
 
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -30,6 +34,12 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
+
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`blog.post`
+    """
+
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
